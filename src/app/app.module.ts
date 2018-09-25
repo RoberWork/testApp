a2e4e2757a0cf1c8cb55b9848b5e64cb21dee60e
Colimasoft <joel.lizardi@colimasoft.com>
2018-09-25 12:32:10 -0500

    Prueba cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,15 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+/*
+
+werwer
+erwwrwerwerwe
+werwrwer
+
+*/
+
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
